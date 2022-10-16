611ca4eb09447a67ae03147475dbd109948bb900
朱洪超 <927994040@qq.com>
2022-10-16 16:37:08 +0800

    9*9

---
+++ 九九乘法表/九九乘法表/99.C
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	int i, j;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (i >= j)
+			{
+				printf("%dX%d=%d	", j, i, i * j);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
