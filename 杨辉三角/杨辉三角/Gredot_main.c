c021a3b208433707b839285ae30cecc87f223b5b
朱洪超 <2377362310@qq.com>
2022-10-20 16:27:24 +0800

    杨辉三角

---
+++ 杨辉三角/杨辉三角/Gredot_main.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	int r, c=1, s;
+	printf("«Î ‰»Î–– ˝£∫");
+	scanf("%d", &r);
+	for (int i = 0; i < r; i++)
+	{
+		for (s = 1; s <= r - i; s++)
+			printf("  ");
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0 || i == 0)
+				c = 1;
+			else
+				c = c * (i - j + 1) / j;
+			printf("%4d", c);
+		}
+		printf("\n");
+	}
+	return 0;
+}
