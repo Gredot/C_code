efeeee4861bd5065632aa7a603e933a5245055e5
朱洪超 <2377362310@qq.com>
2022-10-22 10:02:34 +0800

    将秒数分解成时分秒

---
+++ 秒数转时分秒/秒数转时分秒/Gredot_main.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main ()
+{
+	int seconds;
+	printf("请输入秒数\n");
+	scanf("%d", &seconds);
+	printf("该秒数可以表示成\t%d小时%d分钟%d秒", seconds / 3600, (seconds % 3600) / 60, (seconds % 60));
+	return 0;
+}
