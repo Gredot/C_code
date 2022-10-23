f1c5e931bd041992eae3afc83078bf55191c3d18
朱洪超 <2377362310@qq.com>
2022-10-23 17:00:44 +0800

    判断三角形

---
+++ 10_23_test/10_23_test/Gredot_main.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 0, c = 0;
+    printf("请输入三角形的三边");
+    while ((scanf("%d%d%d", &a, &b, &c)) != EOF)
+    {
+        if ((a + b > c) && (a + c > b) && (b + c > a))
+        {
+            //三角形等腰的三种情况：
+            if ((a == b) && (b != c) || (a==c)&&(a!=b)||(b==c)&&(b!=a))
+                printf("Isosceles triangle!\n");
+            //等边三角形
+            else if ((a == b) && (b == c))
+                printf("Equilateral triangle!\n");
+            //
+            else
+                printf("Ordinary triangle!\n");
+        }
+        else
+            printf("Not a triangle!\n");
+    }
+    return 0;
+}
---
+++ 10_23_test/10_23_test/Gredot_test.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main ()
+//{
+//	int x = 1;
+//	switch (x)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//		printf("hehe");
+//		break;
+//	default :
+//		break;
+//	}
+//	return 0;
+//}
