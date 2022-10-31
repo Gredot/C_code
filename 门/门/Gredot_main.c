976a6437f265d499e8fe299a849a5b93fe7f83ca
朱洪超 <2377362310@qq.com>
2022-10-31 11:50:09 +0800

    题目:某校有N间教室，且每间教室有2扇门，一共有2*N扇门，每扇门都有编号，分别从1-2*N。开始时所有门为关闭状态，。现在按照以下规则对门进行处理： 第一次：将所有的门打开； 第二次：将所有编号为2的倍数的门做相反的处理（开变关，关变开）； 第三次：将所有编号为3的倍数的门做相反的处理； 第N次：将所有编号为N的倍数的门做相反的处理 问N次处理后，有多少扇门为打开状态 N从键盘读入（2<=N<=100） 按照规则队门进行N次处理之后计算有多少扇门为打开状态，并输出

---
+++ 门/门/Gredot_main.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main ()
+{
+	int door[210];
+	int N;
+	scanf("%d", &N);
+	//操作N次
+	for (int i = 1;i<=N;i++)
+	{
+		//对1-N的倍数进行操作
+		//1的倍数  1-N	  An = （An-1） + 1
+		//2的倍数  2 4 6  An = （An-1） + 2
+		//3的倍数  3 6 9  An = （An-1） + 3
+		for (int j = i;j<=2*N;j+=i)
+		{		
+			//对符合的门的状态进行相反处理
+			door[j] = !door[j];
+		}
+	}
+	int count = 0;
+	for (int i = 1; i <= 2 * N; i++)
+	{    //对打开的门进行计数
+		if (door[i])
+			count++;
+	}
+	printf("%d",count);
+	return 0;
+}
