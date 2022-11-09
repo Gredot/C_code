03ae3a1a39355354b428da2db5291750e6f44ef1
朱洪超 <2377362310@qq.com>
2022-11-09 13:40:50 +0800

    选择排序

---
+++ 选择排序/选择排序/Gredot_main.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+void selection_sort(int a[], int len)
+{
+	int i, j, temp;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		int min = i;
+		for (j = i + 1; j < len; j++)     
+		{
+			if (a[j] < a[min])   
+			{
+				min = j;    
+			}
+		}
+		if (min != i)
+		{
+			temp = a[min];  
+			a[min] = a[i];
+			a[i] = temp;
+		}
+	}
+}
+
+int main ()
+{
+	int arr[10];
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d",& arr[i]);
+	}
+	selection_sort(arr,10);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	return 0;
+}
