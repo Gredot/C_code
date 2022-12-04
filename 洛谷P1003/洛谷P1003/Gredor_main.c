65e29ba331e51c523631faa739692843891dfc9b
朱洪超 <2377362310@qq.com>
2022-12-04 19:02:48 +0800

    2022/12/4

--- 洛谷P1003/洛谷P1003/Gredor_main.c
+++ 洛谷P1003/洛谷P1003/Gredor_main.c
@@ -1,22 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+const int MAXN = 10000 + 5;
+int board[MAXN][MAXN] = { 0 };
 int main()
 {
-	int board[100][100] = { 0 };
+	int flg = -1;
 	//地毯数 
 	int n = 0;
-	scanf_s("%d", &n);
+	scanf("%d", &n);
 	//地毯规格信息 
-	int arr[3][4];//该处改成[n][4]即可，vs不支持c99
+	int arr[n][4];//该处改成[n][4]即可，vs不支持c99
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < 4; j++)
-			scanf_s("%d", &arr[i][j]);
+		{
+			scanf("%d", &arr[i][j]);
+		}
 	}
 	//需判断坐标
 	int dx = 0;
 	int dy = 0;
-	scanf_s("%d %d", &dx, &dy);
+	scanf("%d %d", &dx, &dy);
 	//处理地毯规格信息 
 	for (int i = 0; i < n; i++)
 	{
@@ -25,27 +28,10 @@ int main()
 		int  y = arr[i][1];
 		int cx = arr[i][2];
 		int cy = arr[i][3];
-		for (int j = x; j < cx; j++)
-		{
-			for (int k = y; k < cy; k++)
-			{
-				board[j][k] = i + 1;
-			}
-		}
+		if (dx >= x && dy >= y && dx <= cx + x && dy <= cy + y)
+			flg = i + 1;
 	}
-	int flg = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		if (i == board[dx][dy])
-		{
-			printf("%d", i);
-			flg++;
-		}
-	}
-	if (0 == flg)
-	{
-		printf("-1");
-	}
+	printf("%d", flg);
 
 	return 0;
 }
